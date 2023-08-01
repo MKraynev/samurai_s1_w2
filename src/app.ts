73dd18fa1554cd0ebaf7c2815542a8013313a7e0
Матвей Крайнев <matvejkrajnev@iMac-Matvej.local>
2023-08-01 23:14:02 +0200

    вернул всё содержимое в файл app.ts

--- src/app.ts
+++ src/app.ts
@@ -15,4 +15,8 @@ app.use(BlogsPath, blogRouter);
 app.use(PostsPath, postRouter);
 app.use(TestClearAllPath, _TestClearAllRouter);
 
+const PORT: number = 5001;
 
+app.listen(PORT, () => {
+    console.log("app start");
+})
--- src/settings.ts
+++ src/settings.ts
@@ -1,7 +1,2 @@
 import { app } from "./app";
 
-const PORT: number = 5001;
-
-app.listen(PORT, () => {
-    console.log("app start");
-})
